52feee7a9f1110b947c7810f2b1e43d22482eb6c
arth-doming <domingues.arthurusa@gmail.com>
2026-09-15 16:33:49 -0300

    funcao-inserir

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+public class Node{
+    private int  info;
+    private Node esquerda;
+    private Node direita;
+
+    public Node(int info, Node esquerda, Node direita){
+        this.info = info;
+        this.esquerda = esquerda;
+        this.direita = direita;
+    }
+
+    public int getInfo() { return info; }
+    public Node getEsquerda() { return esquerda; }
+    public Node getDireita() { return direita; }
+}
+public class Arvore_Binaria{
+    private Node raiz;
+
+    public Arvore_Binaria(){
+        this.raiz = null;
+    }
+
+    // funcao para inserir elemento
+    public void inserir(int info){
+        raiz = inserirNo(raiz, info);
+    }
+    private Node inserirNo(Node atual, int info){
+        if(atual == null){
+            return new Node(info, null, null );
+        }
+        if (info < atual.info){
+            atual.esquerda = inserirNo(atual.esquerda, info);
+        }
+        if(info >= atual.info){
+            atual.direita = inserirNo(atual.direita, info);
+        }
+        return atual;
+    }
+
+}
+
+void main() {
+    Arvore_Binaria arvore = new Arvore_Binaria();
+
+    arvore.inserir(10);
+    arvore.inserir(20);
+    arvore.inserir(5);
+
+}
